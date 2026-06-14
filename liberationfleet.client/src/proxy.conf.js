057eb8052fab2270ee4c8b6adbd878edd643e777
TruthVoyager <Christianm.jones10999@gmail.com>
2026-06-13 23:53:33 -0400

    updated SDK version to fix build error and fixed other errors.

--- liberationfleet.client/src/proxy.conf.js
+++ liberationfleet.client/src/proxy.conf.js
@@ -1,15 +1,18 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7217';
+  env.ASPNETCORE_HTTP_PORT ? `http://localhost:${env.ASPNETCORE_HTTP_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5157';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
+      "/api"
     ],
     target,
-    secure: false
+    secure: false,
+    changeOrigin: true
   }
 ]
 
